609ae7571b9218ba5bbce60e0472a4704ef3a9a1
carlos  gonzalez <carlos@Mini-de-carlos.bbrouter>
2026-03-15 15:04:44 -0500

    📊 feat: restore Allure reporting for acceptance tests 🚀

--- mc-product-registration/cucumber.js
+++ mc-product-registration/cucumber.js
@@ -3,6 +3,9 @@ module.exports = {
     requireModule: ['ts-node/register', 'tsconfig-paths/register'],
     require: ['tests/acceptance/steps/**/*.ts'],
     paths: ['tests/acceptance/features/**/*.feature'],
-    format: ['progress', 'summary'],
+    format: ['progress', 'summary', 'allure-cucumberjs/reporter'],
+    formatOptions: {
+      resultsDir: './allure-results',
+    },
   },
 };
